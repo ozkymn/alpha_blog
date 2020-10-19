a0df81755f44d4dc43cf65b19b29f7842ea36389
Ozkymn <ozkymn@gmail.com>
2020-10-19 16:36:18 +0300

    Pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,11 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    @article = @user.articles
+    @article = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
   
   def new
@@ -17,7 +21,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Succesfully Update"
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end    
